59b054a1f6602dc0970019cd42b08048efb6f6d3
John Weland <john.weland@gmail.com>
2021-07-25 13:38:12 -0500

    successfully read/write within AWS

--- transcoder.ts
+++ transcoder.ts
@@ -7,18 +7,21 @@
 import { event, dummyEvent } from "./types";
 import ffmpeg_path from "ffmpeg-static";
 import ffmpeg from "fluent-ffmpeg";
-import { S3 } from "aws-sdk";
+import { S3, SharedIniFileCredentials } from "aws-sdk";
 import { Stream } from "stream";
+
+const AWS_CREDENTIALS = new SharedIniFileCredentials({ profile: "default" });
 
 ffmpeg.setFfmpegPath(ffmpeg_path);
 
 /**
- *
+ * @description Upload to AWS S3 in memory
  * @param { string } bucket AWS Destination S3 Bucket Name
  * @param { string } key    AWS Destination S3 Object Key
  * @param { string } tags   AWS Object Tags
  * @param { string } deps   Dependancy Object
- * @returns { Stream.PassThrough } passthrough
+ *
+ * @returns { { Stream.PassThrough, Promise } } object with passthrough && promise
  */
 export const uploadFromStream = (
   bucket: string,
@@ -27,40 +30,73 @@ export const uploadFromStream = (
   deps: { s3: S3 }
 ) => {
   let pass = new Stream.PassThrough();
-  let params = { Bucket: bucket, Key: key, Tagging: tags, Body: pass };
-  deps.s3.upload(params, (err: Error, data: any) => {
-    if (err) console.error(err);
-  });
-  return pass;
+  return {
+    writestream: pass,
+    promise: deps.s3
+      .upload({
+        Bucket: bucket,
+        Key: key,
+        Body: pass,
+        ContentType: "audio/ogg",
+        Tagging: tags,
+      })
+      .promise(),
+  };
 };
 
 /**
- * Core transcoder handler function
+ * @description Core transcoder handler function
  * @param { event } event Incoming S3 event
+ *
+ * @returns { { json } } json {statuscode:200}
  */
-export const handler = (event: event) => {
-  const input_bucket = "string"; // @TODO: pull input_bucket from event
-  const _bucket = "bucket_name"; // @TODO:;
-  const key = "keyname"; // @TODO: pull key from event
-  const _key = key; // @TODO: replace input extension with output extension
+export const handler = async (event: event) => {
+  const input_bucket = event.Records[0].s3.bucket.name;
+  const key = event.Records[0].s3.object.key;
+  const _bucket = "testing-output-s3-bucket";
+  const _key = key.replace(".wav", ".ogg");
   const _tags = "string"; // @TODO: build tagstring from input object tags
   const _deps = {
-    s3: new S3(),
+    s3: new S3({
+      httpOptions: {
+        timeout: 30000,
+      },
+      maxRetries: 3,
+      credentials: AWS_CREDENTIALS,
+    }),
   };
-  const input_url = `https://${input_bucket}.s3.amazonaws.com/${key}`;
-  const writestream = uploadFromStream(_bucket, _key, _tags, _deps);
+  const input_url = `https://${input_bucket}.s3.us-west-2.amazonaws.com/${key}`;
 
-  ffmpeg(input_url) // @TODO: replace with S3 URL built from ex https://input_bucket/stuff.region.aws.com/key
+  // @TODO: Detect is the input object exists before proceeding
+
+  const { writestream, promise } = uploadFromStream(
+    _bucket,
+    _key,
+    _tags,
+    _deps
+  );
+
+  ffmpeg(input_url)
     .noVideo()
     .audioBitrate(128)
     .audioCodec("libvorbis")
-    .on("error", (err) => {
+    .format("ogg")
+    .on("error", (err: Error) => {
       console.log("An error occurred: " + err.message);
     })
     .on("end", () => {
       console.log("Finished processing");
     })
-    .pipe(writestream);
+    .pipe(writestream, {
+      end: true,
+    });
+  let output = { statusCode: 200 };
+  await promise.catch((err) => {
+    output = { statusCode: 500 };
+    console.error(err.message);
+  });
+
+  return output;
 };
 
 // Uncomment to fire locally with a simulated event.
--- types.ts
+++ types.ts
@@ -1,10 +1,35 @@
-
-
+import { String } from "aws-sdk/clients/batch";
 
 export type event = {
-    [x: string]: string
-}
+  Records: [
+    {
+      s3: {
+        s3SchemaVersion?: string;
+        configurationId?: string;
+        bucket: {
+          name: string;
+          arn: string;
+        };
+        object: {
+          key: string;
+        };
+      };
+    }
+  ];
+};
 
 export const dummyEvent = {
-    "test": "fire"
-}
+  Records: [
+    {
+      s3: {
+        bucket: {
+          name: "test-ingest-s3-bucket",
+          arn: "arn:aws:s3:::test-ingest-s3-bucket",
+        },
+        object: {
+          key: "file_example_WAV_10MG.wav",
+        },
+      },
+    },
+  ],
+};
